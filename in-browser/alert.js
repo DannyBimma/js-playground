f140ec5d494fb4f67de0ecf65258fdf8d25c82f9
DannyBimma <dantrotman92@gmail.com>
2025-09-12 21:19:04 -0400

    Use multi-line comment, be pendantic and proudga

--- in-browser/alert.js
+++ in-browser/alert.js
@@ -1,6 +1,8 @@
-// JavaScript.info task: Create a page that shows an alert in an
-// external script.
-//
+/*
+ * JavaScript.info task: Create a page that shows an alert in an
+ * external script
+ */
+
 alert("Hello, World! I am JavaScript!!");
 
 console.log("Playground initialised successfully, bitch!");
