6235a8b4fb13a3eecdbf151f120f8ed0216591f4
DannyBimma <dantrotman92@gmail.com>
2025-10-03 21:42:12 -0400

    Output change.

--- basics/bmi.js
+++ basics/bmi.js
@@ -4,16 +4,16 @@ const massMark = 95;
 const heightMark = 1.88;
 const massJohn = 85;
 const heightJohn = 1.76;
-let markHigherBMI = false;
 
 const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 
-console.log(BMIMark);
-console.log(BMIJohn);
-
 if (BMIMark > BMIJohn) {
-  markHigherBMI = true;
+  console.log(
+    `Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})!`,
+  );
+} else {
+  console.log(
+    `John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark})!`,
+  );
 }
-
-console.log(markHigherBMI);
