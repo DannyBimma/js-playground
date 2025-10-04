4c1df322bb6759f3c207756f6ce0a4dcd6de3bd2
DannyBimma <dantrotman92@gmail.com>
2025-10-03 20:41:12 -0400

    I see.

--- basics/bmi.js
+++ basics/bmi.js
@@ -1,1 +1,19 @@
 // Variables, control-flow, operator precedence?
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+let markHigherBMI = false;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  markHigherBMI = true;
+}
+
+console.log(markHigherBMI);
