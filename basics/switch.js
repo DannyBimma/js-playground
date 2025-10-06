12d53beddcbcc6de3536199b36f1dbaff04c564a
DannyBimma <dantrotman92@gmail.com>
2025-10-05 21:09:17 -0400

    For instead of Switch.

---
+++ basics/switch.js
@@ -0,0 +1,19 @@
+// Switch Statement Example
+
+const day = "Sunday";
+
+if (day === "Monday") {
+  console.log("That's a sad day.");
+} else if (day === "Tuesday") {
+  console.log("That's a fools day.");
+} else if (day === "Wednesday") {
+  console.log("That's a humping day.");
+} else if (day === "Thursday") {
+  console.log("That's a relatively worse day.");
+} else if (day === "Friday") {
+  console.log("That's the right day.");
+} else if (day === "Saturday") {
+  console.log("That's an even better day");
+} else {
+  console.log("That's a fun mother-fucking day.");
+}
