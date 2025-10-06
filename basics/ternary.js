60e0d6bf2b7c8f82e662c0d032125b3fae850932
DannyBimma <dantrotman92@gmail.com>
2025-10-05 22:06:22 -0400

    Do it.

--- basics/ternary.js
+++ basics/ternary.js
@@ -1,3 +1,9 @@
-// Ternary Operator Example
+// CHALLENGE 4: Ternary Operator Example
 
 const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const total = bill + tip;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${total}.`,
+);
